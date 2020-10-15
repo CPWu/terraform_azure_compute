161f99b90ea140a1e92d18aa4e82d0d9c5d2210c
Chun Wu <the.chun.wu@gmail.com>
2020-10-15 17:05:26 +0800

    Added vnet

--- main.tf
+++ main.tf
@@ -5,86 +5,86 @@ resource "azurerm_resource_group" "sandbox" {
 
  resource "azurerm_virtual_network" "sandbox_vnet" {
     name                                = "${var.RESOURCE_GROUP_NAME}-vnet"
-    location                            = "${var.AZURE_REGION}"   
-    resource_group_name                 = "${var.RESOURCE_GROUP_NAME}"
-    address_space                       = ["${var.VNET_ADDRESS_SPACE}"]
+    location                            = var.AZURE_REGION
+    resource_group_name                 = var.RESOURCE_GROUP_NAME
+    address_space                       = [var.VNET_ADDRESS_SPACE]
 }
 
-resource "azurerm_subnet" "sandbox_subnet" {
-    name                                = "${var.RESOURCE_GROUP_NAME}-subnet"
-    resource_group_name                 = "${var.RESOURCE_GROUP_NAME}"
-    virtual_network_name                = "${azurerm_virtual_network.sandbox_vnet.name}"
-    address_prefix                      = "${var.SUBNET_ADDRESS_PREFIX}"    
-}
+# resource "azurerm_subnet" "sandbox_subnet" {
+#     name                                = "${var.RESOURCE_GROUP_NAME}-subnet"
+#     resource_group_name                 = "${var.RESOURCE_GROUP_NAME}"
+#     virtual_network_name                = "${azurerm_virtual_network.sandbox_vnet.name}"
+#     address_prefix                      = "${var.SUBNET_ADDRESS_PREFIX}"    
+# }
 
-resource "azurerm_public_ip" "Server-public-ip" {
-  name                                        = "${var.RESOURCE_GROUP_NAME}-public-ip"
-  location                                    = "${var.AZURE_REGION}"
-  resource_group_name                         = "${var.RESOURCE_GROUP_NAME}"
-  allocation_method                           = "${var.IP_TYPE}"    
-}
-resource "azurerm_network_interface" "Server-DevTest-nic" {
-    name                                = "${var.SERVER_NAME}-nic"
-    location                            = "${var.AZURE_REGION}"
-    resource_group_name                 = "${var.RESOURCE_GROUP_NAME}"
-    network_security_group_id           = "${azurerm_network_security_group.Server_nsg.id}"
+# resource "azurerm_public_ip" "Server-public-ip" {
+#   name                                        = "${var.RESOURCE_GROUP_NAME}-public-ip"
+#   location                                    = "${var.AZURE_REGION}"
+#   resource_group_name                         = "${var.RESOURCE_GROUP_NAME}"
+#   allocation_method                           = "${var.IP_TYPE}"    
+# }
+# resource "azurerm_network_interface" "Server-DevTest-nic" {
+#     name                                = "${var.SERVER_NAME}-nic"
+#     location                            = "${var.AZURE_REGION}"
+#     resource_group_name                 = "${var.RESOURCE_GROUP_NAME}"
+#     network_security_group_id           = "${azurerm_network_security_group.Server_nsg.id}"
 
-    ip_configuration {
-        name                                      = "${var.SERVER_NAME}-ip"  
-        subnet_id                                 = "${azurerm_subnet.Space_DevTest_subnet.id}"
-        private_ip_address_allocation             = "dynamic"
-        public_ip_address_id                      = "${azurerm_public_ip.Server-public-ip.id}"        
-    }    
-}
+#     ip_configuration {
+#         name                                      = "${var.SERVER_NAME}-ip"  
+#         subnet_id                                 = "${azurerm_subnet.Space_DevTest_subnet.id}"
+#         private_ip_address_allocation             = "dynamic"
+#         public_ip_address_id                      = "${azurerm_public_ip.Server-public-ip.id}"        
+#     }    
+# }
 
-resource "azurerm_network_security_group" "Server_nsg" {
-name                                      = "${var.SERVER_NAME}-nsg"
-location                                  = "${var.AZURE_REGION}"
-resource_group_name                       = "${var.RESOURCE_GROUP_NAME}"
-}
+# resource "azurerm_network_security_group" "Server_nsg" {
+# name                                      = "${var.SERVER_NAME}-nsg"
+# location                                  = "${var.AZURE_REGION}"
+# resource_group_name                       = "${var.RESOURCE_GROUP_NAME}"
+# }
 
-resource "azurerm_network_security_rule" "Server_nsg_rule_rdp" {
-  name                                        = "SSH"
-  priority                                    = 100
-  direction                                   = "Inbound"
-  access                                      = "Allow"
-  protocol                                    = "TCP"
-  source_port_range                           = "*"
-  destination_port_range                      = "22"
-  source_address_prefix                       = "*"
-  destination_address_prefix                  = "*"
-  resource_group_name                         = "${var.RESOURCE_GROUP_NAME}"
-  network_security_group_name                 = "${azurerm_network_security_group.Server_nsg.name}"
-}
+# resource "azurerm_network_security_rule" "Server_nsg_rule_rdp" {
+#   name                                        = "SSH"
+#   priority                                    = 100
+#   direction                                   = "Inbound"
+#   access                                      = "Allow"
+#   protocol                                    = "TCP"
+#   source_port_range                           = "*"
+#   destination_port_range                      = "22"
+#   source_address_prefix                       = "*"
+#   destination_address_prefix                  = "*"
+#   resource_group_name                         = "${var.RESOURCE_GROUP_NAME}"
+#   network_security_group_name                 = "${azurerm_network_security_group.Server_nsg.name}"
+# }
 
-resource "azurerm_virtual_machine" "jumpserver" {
-  name                                        = "${var.SERVER_NAME}"
-  location                                    = "${var.AZURE_REGION}"
-  resource_group_name                         = "${azurerm_resource_group.DevTest_rg.name}"
-  network_interface_ids                       = ["${azurerm_network_interface.Server-DevTest-nic.id}"]
-  vm_size                                     = "Standard_B1s"
+# resource "azurerm_virtual_machine" "jumpserver" {
+#   name                                        = "${var.SERVER_NAME}"
+#   location                                    = "${var.AZURE_REGION}"
+#   resource_group_name                         = "${azurerm_resource_group.DevTest_rg.name}"
+#   network_interface_ids                       = ["${azurerm_network_interface.Server-DevTest-nic.id}"]
+#   vm_size                                     = "Standard_B1s"
 
-  storage_image_reference {
-    publisher                                 = "Canonical"
-    offer                                     = "UbuntuServer"
-    sku                                       = "16.04.0-LTS"
-    version                                   = "latest"
-  }
+#   storage_image_reference {
+#     publisher                                 = "Canonical"
+#     offer                                     = "UbuntuServer"
+#     sku                                       = "16.04.0-LTS"
+#     version                                   = "latest"
+#   }
 
-  storage_os_disk {
-    name                                      = "${var.SERVER_NAME}-os-disk"
-    caching                                   = "ReadWrite"
-    create_option                             = "FromImage"
-    managed_disk_type                         = "Standard_LRS"
-  }
+#   storage_os_disk {
+#     name                                      = "${var.SERVER_NAME}-os-disk"
+#     caching                                   = "ReadWrite"
+#     create_option                             = "FromImage"
+#     managed_disk_type                         = "Standard_LRS"
+#   }
 
-  os_profile {
-    computer_name                             = "${var.SERVER_NAME}"    
-    admin_username                            = "testuser"
-    admin_password                            = "Passw0rd!"
-  }
+#   os_profile {
+#     computer_name                             = "${var.SERVER_NAME}"    
+#     admin_username                            = "testuser"
+#     admin_password                            = "Passw0rd!"
+#   }
 
-  os_profile_linux_config {
-      disable_password_authentication = false
-  }
-}
+#   os_profile_linux_config {
+#       disable_password_authentication = false
+#   }
+# }
